7375d5e8b08d04bfdd73dd7f85c4e2bf19cf52d8
Dipesh Bajgain <dipbazz@gmail.com>
2021-01-28 20:26:41 +0545

    Add global methods to check for user session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,6 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    if session[:current_user_id]
-     @user = User.find(session[:current_user_id])
-    else
-      @user = nil
-    end
   end
 
   # GET /users/1 or /users/1.json
@@ -16,25 +11,22 @@ class UsersController < ApplicationController
 
   # GET /users/new
   def new
+    if session[:current_user_id]
+      flash[:notice] = "You are already logged in."
+      return redirect_to users_path
+    end
     @user = User.new
   end
-
-  
 
 
   # POST /users or /users.json
   def create
-    @user = User.new(user_params)
+    @user = User.find_or_create_by(user_params)
 
     respond_to do |format|
-      if @user.save 
-       session[:current_user_id] = @user.id
-        format.html { redirect_to @user, notice: "User was successfully created." }
+        session[:current_user_id] = @user.id
+        format.html { redirect_to @user, notice: "You are logged in successfully." }
         format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
     end
   end
 
@@ -43,7 +35,7 @@ class UsersController < ApplicationController
   def destroy
     session[:current_user_id] = nil
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.html { redirect_to users_url, notice: "You are successfully logged out." }
       format.json { head :no_content }
     end
   end

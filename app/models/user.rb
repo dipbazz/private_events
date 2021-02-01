390208723455b5331fa1ded64f5fc63dd3b16fbd
Your Name <basaijapascal9@gmail.com>
2021-02-01 17:08:03 +0300

    Modify users show page and Events show page to;
    Upcoming events from previous ones
    Apply scopes to past and upcoming methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
+
 class User < ApplicationRecord
     has_many :events, foreign_key: :creator_id
     has_many :attendences, foreign_key: :attendee_id
     has_many :attended_events, through: :attendences
+
+    
+
+    def upcoming_events
+        self.attended_events.upcoming_events
+    end
+
+    def previous_events
+        self.attended_events.previous_events
+    end
 end

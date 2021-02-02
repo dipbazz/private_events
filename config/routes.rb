df04c409e73dddef5e5d20bb47a2855f113406eb
Dipesh Bajgain <dipbazz@gmail.com>
2021-02-02 14:31:14 +0545

    Update style of the project and fix some bug
    add bootstrap gem file and update the index page of events
    to show the events in card layout for upcomming and previous
    events. Add navabar and alert for flash message in
    application.html.erb file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'events#index'
+
   resources :users
   resources :events, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
